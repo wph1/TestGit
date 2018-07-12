fb2b9e005b9383c6c59c3d347fbed211cb70c7c6
wangpenghao <wphph_123456@126.com>
2018-07-12 15:19:39 +0800

    水电费第三方

--- wph.java
+++ wph.java
@@ -4,16 +4,16 @@ main{
 
 win7{
 	system.out.println("win7上面进行保存！！！");
-<<<<<<< HEAD
+
 }win777{
 	system.out.println("win7777上面进行保存！！！");
 
 }
 win10{
 	system.out.println("win10上面进行保存！！！");
-=======
+
 }
 win777{
 	system.out.println("win7777上面进行保存！！！");
->>>>>>> cd119bfd6618db16e40439fb4d3500554778931d
+
 }
